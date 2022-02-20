4f7c6f5e76ca8f0ef4c7a45537fe7c320ef770a4
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2022-02-20 21:14:35 +0000

    text input event type handler

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Greet } from './components/Greet';
 import { Person } from './components/Person';
 import { PersonList } from './components/Personlist';
 import { Button } from './components/Button';
+import { Input } from './components/Input';
 
 function App() {
   const personName = {
@@ -28,6 +29,10 @@ function App() {
       <Greet name={'Jane'} messageCount={20} isLoggedIn={true} />
       <Person name={personName} />
       <PersonList names={nameList} />
+      <Input
+        value=''
+        handleChange={(event) => console.log(event.target.value)}
+      />
       <Button
         handleClick={(event, id) => {
           console.log('Button clicked', event, 'id: ', id);
---
+++ src/components/Input.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+type InputProps = {
+  value: string;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export const Input = (props: InputProps) => {
+  return (
+    <div>
+      <input
+        type='text'
+        value={props.value}
+        onChange={props.handleChange}
+      ></input>
+    </div>
+  );
+};
