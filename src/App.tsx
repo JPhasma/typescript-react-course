7157403e00e89e819944874757e1862cc85c711b
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2022-02-20 17:59:43 +0000

    typing props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import { Greet } from './components/Greet';
 
 function App() {
   return (
     <div className='App'>
       <h1>REACT | TYPESCRIPT</h1>
+      <Greet name='Jane' />
     </div>
   );
 }
