cb25fe8dc5ec57b138295f5adbca8b9baf53fd1d
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2022-02-20 18:18:34 +0000

    basic and object props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css';
 import { Greet } from './components/Greet';
+import { Person } from './components/Person';
 
 function App() {
+  const personName = {
+    first: 'Kory',
+    last: 'Anders',
+  };
   return (
     <div className='App'>
       <h1>REACT | TYPESCRIPT</h1>
-      <Greet name='Jane' />
+      <Greet name={'Jane'} messageCount={20} isLoggedIn={false} />
+      <Person name={personName} />
     </div>
   );
 }
